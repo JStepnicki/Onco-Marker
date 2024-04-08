ec98e49cd0ce1e90460278780f4357600644c9f0
wzyndul <242575@edu.p.lodz.pl>
2024-04-08 18:15:54 +0200

    feat: data model

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,3 +7,30 @@ class Test(models.Model):
 
     def __str__(self):
         return f"Test: {self.title}"
+    
+class Cancer_Sample(models.Model):
+    organ_type = models.CharField(max_length=255)
+    patient_cohort = models.CharField(max_length=255)
+    sample_origin = models.CharField(max_length=255)
+    markers_JSON = models.TextField()
+
+    def set_markers(self, markers):
+        self.markers_JSON = json.dumps(markers)
+
+    def get_markers(self):
+        return json.loads(self.markers_JSON)
+
+
+
+class Patient(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    age = models.IntegerField()
+    sex = models.BooleanField()
+    cancer_samples = models.ManyToManyField(Cancer_Sample)
+
+
+
+ 
+
+
