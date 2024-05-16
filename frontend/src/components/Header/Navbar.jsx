50e61149886abe3940d3d0b64b04a6bf4ba33a30
JStepnicki <j.stepnicki2002@gmail.com>
2024-05-16 22:25:13 +0200

    Update Navbar.jsx

--- frontend/src/components/Header/Navbar.jsx
+++ frontend/src/components/Header/Navbar.jsx
@@ -1,19 +1,32 @@
 import { styled } from '@mui/system';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
+import axios from 'axios';
 
 const StyledAppBar = styled(AppBar)({
   backgroundColor: '#333',
+  display: 'flex',
+  justifyContent: 'space-between',
 });
 
-
-
 function Navbar() {
+  const handleLogout = async () => {
+    try {
+      await axios.post('http://127.0.0.1:8000/api/logout/');
+        window.location.href = '/';
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <StyledAppBar position="static">
       <Toolbar>
-        <Button color="inherit" component={RouterLink} to="/">Main Page</Button>
-        <Button color="inherit" component={RouterLink} to="/doctors">Doctor Page</Button>
+        <div>
+          <Button color="inherit" component={RouterLink} to="/">Main Page</Button>
+          <Button color="inherit" component={RouterLink} to="/doctors">Doctor Page</Button>
+        </div>
+        <Button color="inherit" onClick={handleLogout}>Logout</Button>
       </Toolbar>
     </StyledAppBar>
   );
