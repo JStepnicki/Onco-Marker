afd3d5125ea33cf31eb9966e30d5676d37b288c6
MushroomArturek <artur.grzybek@gmail.com>
2024-06-06 17:48:42 +0200

    feat: Two tests left

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,12 +1,16 @@
 import json
 import uuid
 
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import PasswordResetForm
+from django.test.client import RequestFactory
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 from api.models import Patient, CancerSample
 from api.serializers import CancerSampleSerializer
+from api.views import reset_password
 
 class TestPatientViews(TestCase):
     def setUp(self):
@@ -330,3 +334,179 @@ class TestPatientViews(TestCase):
         response = self.client.get(url, {'sample_id': 9999})
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_patient_cancer_samples(self):
+        patient = Patient.objects.create(name='John', surname='Doe', age=33, sex=1, email='john.doe@example.com')
+        sample1 = CancerSample.objects.create(patient=patient, stage="", benign_sample_diagnosis="", markers_JSON={},
+                                              diagnosis="", organ_type="")
+        sample2 = CancerSample.objects.create(patient=patient, stage="", benign_sample_diagnosis="", markers_JSON={},
+                                              diagnosis="", organ_type="")
+
+        url = reverse('get-patient-cancer-samples', kwargs={'pk': patient.pk})
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.data
+
+        expected_data = CancerSampleSerializer(instance=[sample1, sample2], many=True).data
+        self.assertEqual(data, expected_data)
+
+    def test_get_patient_cancer_samples_not_found(self):
+        url = reverse('get-patient-cancer-samples', kwargs={'pk': 9999})
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_all_cancer_samples(self):
+        patient = Patient.objects.create(name='John', surname='Doe', age=33, sex=1, email='john.doe@example.com')
+
+        sample1 = CancerSample.objects.create(patient=patient, stage="", benign_sample_diagnosis="", markers_JSON={}, diagnosis="", organ_type="")
+        sample2 = CancerSample.objects.create(patient=patient, stage="", benign_sample_diagnosis="", markers_JSON={}, diagnosis="", organ_type="")
+
+        url = reverse('all_results')
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.data
+
+        expected_data = CancerSampleSerializer(instance=[sample1, sample2], many=True).data
+        self.assertEqual(data, expected_data)
+
+    def test_patient_results(self):
+        access_token = uuid.uuid4()
+
+        patient = Patient.objects.create(
+            access_token=access_token,
+            age=30,
+            sex=1
+        )
+
+        sample1 = CancerSample.objects.create(patient=patient, stage="", benign_sample_diagnosis="", markers_JSON={},
+                                              diagnosis="", organ_type="")
+        sample2 = CancerSample.objects.create(patient=patient, stage="", benign_sample_diagnosis="", markers_JSON={},
+                                              diagnosis="", organ_type="")
+
+        url = reverse('patient_results', kwargs={'access_token': access_token})
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.data
+
+        expected_data = CancerSampleSerializer(instance=[sample1, sample2], many=True).data
+
+        self.assertEqual(data, expected_data)
+
+    def test_register_valid_data(self):
+        url = reverse('register')
+        data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password': 'testpassword'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_register_invalid_data(self):
+        url = reverse('register')
+        data = {
+            'username': '',
+            'email': 'test@example.com',
+            'password': 'testpassword'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_register_valid_data(self):
+        url = reverse('register')
+        data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password': 'testpassword'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_login_valid_credentials(self):
+        # Tworzymy użytkownika bezpośrednio przed testem
+        username = 'testuser'
+        password = 'testpassword'
+        user = User.objects.create_user(username=username, password=password)
+
+        url = reverse('login')
+        data = {
+            'username': username,
+            'password': password
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_login_invalid_credentials(self):
+        # Tworzymy użytkownika bezpośrednio przed testem
+        username = 'testuser'
+        password = 'testpassword'
+        user = User.objects.create_user(username=username, password=password)
+
+        url = reverse('login')
+        data = {
+            'username': username,
+            'password': 'wrongpassword'  # Nieprawidłowe hasło
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_user_logout(self):
+        self.client.login(username='testuser', password='testpassword')
+
+        url = reverse('logout')
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_user_authenticated(self):
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+
+        url = reverse('user')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_user_unauthenticated(self):
+        url = reverse('user')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_reset_password_valid_email(self):
+        client = APIClient()
+        factory = RequestFactory()
+        url = reverse('reset_password')
+        email = 'test@example.com'
+
+        request = factory.post(url, {'email': email})
+
+        request.is_secure = lambda: True
+
+        response = reset_password(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('message', json.loads(response.content.decode()))
+
+    def test_reset_password_invalid_email(self):
+        client = APIClient()
+        factory = RequestFactory()
+        url = reverse('reset_password')
+        email = 'invalid_email'
+
+        request = factory.post(url, {'email': email})
+
+        response = reset_password(request)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('error', json.loads(response.content.decode()))
