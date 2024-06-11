356d2dd396bf49b4f707a11fdf5f561c4a54005b
JStepnicki <j.stepnicki2002@gmail.com>
2024-06-11 13:52:45 +0200

    Update tests.py

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -522,10 +522,9 @@ from api.views import classify_sample
 class TestClassifySample(TestCase):
     @classmethod
     def setUpTestData(cls):
-        # Utwórz przykładowe próbki do testów
         cls.sample1 = CancerSample.objects.create(
             diagnosis="1",
-            organ_type="pancreas",
+            organ_type="test",
             stage = None,
             markers_JSON={
                 "marker1": 1.0,
@@ -535,7 +534,7 @@ class TestClassifySample(TestCase):
 
         cls.sample2 = CancerSample.objects.create(
             diagnosis="2",
-            organ_type="pancreas",
+            organ_type="test",
             stage=None,
             markers_JSON={
                 "marker1": 2.0,
@@ -546,7 +545,7 @@ class TestClassifySample(TestCase):
         cls.sample3 = CancerSample.objects.create(
             diagnosis="3",
             stage= "IIIB",
-            organ_type="pancreas",
+            organ_type="test",
             markers_JSON={
                 "marker1": 3.0,
                 "marker2": 4.0
@@ -555,7 +554,7 @@ class TestClassifySample(TestCase):
         cls.sample9 = CancerSample.objects.create(
             diagnosis="3",
             stage="IIIB",
-            organ_type="pancreas",
+            organ_type="test",
             markers_JSON={
                 "marker1": 3.0,
                 "marker2": 4.0
@@ -564,7 +563,7 @@ class TestClassifySample(TestCase):
         cls.sample10 = CancerSample.objects.create(
             diagnosis="3",
             stage="IIIB",
-            organ_type="pancreas",
+            organ_type="test",
             markers_JSON={
                 "marker1": 3.0,
                 "marker2": 4.0
@@ -573,7 +572,7 @@ class TestClassifySample(TestCase):
         cls.sample11 = CancerSample.objects.create(
             diagnosis="3",
             stage="IIIB",
-            organ_type="pancreas",
+            organ_type="test",
             markers_JSON={
                 "marker1": 3.0,
                 "marker2": 4.0
@@ -582,7 +581,7 @@ class TestClassifySample(TestCase):
         cls.sample4 = CancerSample.objects.create(
             diagnosis="1",
             stage=None,
-            organ_type="pancreas",
+            organ_type="test",
             markers_JSON={
                 "marker1": 4.0,
                 "marker2": 5.0
@@ -592,7 +591,7 @@ class TestClassifySample(TestCase):
         cls.sample5 = CancerSample.objects.create(
             diagnosis="2",
             stage=None,
-            organ_type="pancreas",
+            organ_type="test",
             markers_JSON={
                 "marker1": 5.0,
                 "marker2": 6.0
@@ -602,7 +601,7 @@ class TestClassifySample(TestCase):
         cls.sample6 = CancerSample.objects.create(
             diagnosis="1",
             stage=None,
-            organ_type="pancreas",
+            organ_type="test",
             markers_JSON={
                 "marker1": 6.0,
                 "marker2": 7.0
@@ -611,35 +610,20 @@ class TestClassifySample(TestCase):
 
     @patch('api.views.CancerSample.objects.get')
     def test_classify_sample_existing_sample(self, mock_get):
-        # Skonfiguruj metodę mock_get, aby zwracała przykładową próbkę
         mock_get.return_value = self.sample1
-
-        # Wywołaj funkcję classify_sample
-        y_pred, stage_pred = classify_sample(self.sample1.id, "pancreas")
-
-        # Sprawdź, czy funkcja zwraca poprawne wyniki
+        y_pred, stage_pred = classify_sample(self.sample1.id, "test")
         self.assertEqual(y_pred, "1")
         self.assertIsNone(stage_pred)
 
     @patch('api.views.CancerSample.objects.get')
     def test_classify_sample_missing_sample(self, mock_get):
-        # Skonfiguruj metodę mock_get, aby zgłaszała wyjątek CancerSample.DoesNotExist
         mock_get.side_effect = CancerSample.DoesNotExist
-
-        # Wywołaj funkcję classify_sample
-        result = classify_sample(999, "pancreas")
-
-        # Sprawdź, czy funkcja zwraca komunikat o błędzie
+        result = classify_sample(999, "test")
         self.assertIn("error", result)
 
     @patch('api.views.CancerSample.objects.get')
     def test_classify_sample_diagnosis_3(self, mock_get):
-        # Skonfiguruj metodę mock_get, aby zwracała próbkę z diagnozą "3"
         mock_get.return_value = self.sample3
-
-        # Wywołaj funkcję classify_sample
-        y_pred, stage_pred = classify_sample(self.sample3.id, "pancreas")
-
-        # Sprawdź, czy funkcja zwraca poprawne wyniki
+        y_pred, stage_pred = classify_sample(self.sample3.id, "test")
         self.assertEqual(y_pred, "3")
         self.assertEqual(stage_pred,"IIIB")
