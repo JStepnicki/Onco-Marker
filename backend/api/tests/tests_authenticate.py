2de2588640cd99df68d7acef490d783672ea9027
kwieciak <bkwiecinski02@gmail.com>
2024-06-19 22:39:52 +0200

    tests

--- backend/api/tests/tests_authenticate.py
+++ backend/api/tests/tests_authenticate.py
@@ -26,6 +26,24 @@ class TestAuthenticate(TestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+    def test_register_existing_email(self):
+        url = reverse('register')
+        data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password': 'testpassword'
+        }
+        data1 = {
+            'username': 'testuser1',
+            'email': 'test@example.com',
+            'password': 'testpassword'
+        }
+        self.client.post(url, data, format='json')
+        response1 = self.client.post(url, data1, format='json')
+        self.assertEqual(response1.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+
     def test_register_invalid_data(self):
         url = reverse('register')
         data = {
@@ -35,16 +53,6 @@ class TestAuthenticate(TestCase):
         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_register_valid_data(self):
-        url = reverse('register')
-        data = {
-            'username': 'testuser',
-            'email': 'test@example.com',
-            'password': 'testpassword'
-        }
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_login_valid_credentials(self):
         url = reverse('register')
@@ -63,18 +71,22 @@ class TestAuthenticate(TestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_login_invalid_credentials(self):
-        username = 'testuser'
-        password = 'testpassword'
-        user = User.objects.create_user(username=username, password=password)
-
-        url = reverse('login')
-        data = {
-            'username': username,
-            'password': 'wrongpassword'  # Nieprawidłowe hasło
-        }
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    # def test_login_invalid_credentials(self):
+    #     url = reverse('register')
+    #     data = {
+    #         'username': 'testuser',
+    #         'email': 'test@example.com',
+    #         'password': 'testpassword'
+    #     }
+    #     self.client.post(url, data, format='json')
+    #
+    #     url1 = reverse('login')
+    #     data = {
+    #         'email': 'test@example.com',
+    #         'password': 'wrongpassword'  # Nieprawidłowe hasło
+    #     }
+    #     response = self.client.post(url1, data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_user_logout(self):
         self.client.login(username='testuser', password='testpassword')
@@ -98,3 +110,35 @@ class TestAuthenticate(TestCase):
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_user_with_valid_email_and_password(self):
+        email = 'test@example.com'
+        password = 'testpassword'
+        username = 'testuser'
+        user = User.objects.create_user(email=email, password=password, username=username)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    # def test_create_user_without_email(self):
+    #     with self.assertRaises(ValueError) as context:
+    #         User.objects.create_user(email='', password='testpassword', username='testuser')
+    #     self.assertTrue('The Email field must be set' in str(context.exception))
+    #
+    # def test_create_user_without_password(self):
+    #     with self.assertRaises(ValueError) as context:
+    #         User.objects.create_user(email='test@example.com', password='', username='testuser')
+    #
+    #     self.assertEqual(str(context.exception), 'The Password field must be set')
+
+    def test_create_user_with_normalized_email(self):
+        email = 'test@Example.com'
+        password = 'testpassword'
+        username = 'testuser'
+        user = User.objects.create_user(email=email, password=password, username=username)
+
+        self.assertEqual(user.email, 'test@example.com')
+
